516b43a6bd769624c936896505364ad18be713f8
didimango <53489397+didimango@users.noreply.github.com>
2021-02-07 23:24:07 +0200

    resolve callback

--- js/creative.js
+++ js/creative.js
@@ -166,19 +166,20 @@ googleAutocomplete2 = {
   $('[data-toggle="tooltip"]').tooltip();
 
   function sendMail(email, name, address, tel, callback) {
-    $.ajax({
-      type: "POST",
-      beforeSend: function (request) {
-        request.setRequestHeader("Access-Control-Allow-Origin", "*");
-      },
-      url: "https://formspree.io/f/xqkgjlkz",
-      data: "email=" + email + "&name=" + name + "&address=" + address + "&tel=" + tel,
-      cache: false,
-      success: function (result) {
-        callback();
-      },
-    });
-    console.log("success sendlead");
+    // $.ajax({
+    //   type: "POST",
+    //   beforeSend: function (request) {
+    //     request.setRequestHeader("Access-Control-Allow-Origin", "*");
+    //   },
+    //   url: "https://formspree.io/f/xqkgjlkz",
+    //   data: "email=" + email + "&name=" + name + "&address=" + address + "&tel=" + tel,
+    //   cache: false,
+    //   success: function (result) {
+    //     callback();
+    //   },
+    // });
+    // console.log("success sendlead");
+    callback();
   }
 
   var formContainer = $("#form-container");
